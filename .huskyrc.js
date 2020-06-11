e21bda816446959cdbf1de1e1cff522fb0bf97c1
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-11 23:48:35 +0900

    fix: huskyrc

--- .huskyrc.js
+++
@@ -1,9 +0,0 @@
-const tasks = arr => arr.join(' && ')
-module.exports = {
-  'scripts': {
-    'pre-commit': tasks([
-      "lint-staged",
-    ]),
-    "commit-msg": "commitlint -e $HUSKY_GIT_PARAMS"
-  }
-}
